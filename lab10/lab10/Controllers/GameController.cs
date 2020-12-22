c4a61cc83d54da122ec66f3baa8ecc6cfe91a15e
joannasroka <joannasroka98@gmail.com>
2020-12-22 22:17:15 +0100

    MVC web application

---
+++ lab10/lab10/Controllers/GameController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab10.Controllers
+{
+    public enum GuessingResult
+    {
+        TooSmall,
+        Perfect,
+        TooLarge
+    }
+
+    public class GameController : Controller
+    {
+        private static int drawnNumber;
+        private static int n;
+        private static Random rand = new Random();
+        private static Boolean nCorrectSelection = false;
+        private static Boolean drawn = false;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Set(int n)
+        {
+            if (n <= 0)
+            {
+                nCorrectSelection = false;
+                ViewBag.errorMsg = "Zakres musi byc liczba wieksza od 0. Sprobuj ponownie.";
+            }
+            else
+            {
+                ViewBag.errorMsg = "";
+                nCorrectSelection = true;
+                GameController.n = n;
+                ViewBag.n = n;
+            }
+
+            drawn = false;
+            return View("Set");
+        }
+
+        public IActionResult Draw()
+        {
+            if (nCorrectSelection)
+            {
+                ViewBag.errorMsg = "";
+                drawnNumber = rand.Next(n);
+                ViewBag.n = n - 1;
+                drawn = true;
+            }
+            else
+            {
+                drawn = false;
+                ViewBag.errorMsg = "Najpierw ustal poprawny zakres.";
+            }
+
+            return View("Draw");
+        }
+
+        public IActionResult Guess(int guessedNumber)
+        {
+            if (!nCorrectSelection)
+            {
+                ViewBag.errorMsg = "Najpierw ustal poprawny zakres.";
+            }
+            else if (!drawn)
+            {
+                ViewBag.errorMsg = "Najpierw wylosuj liczbe.";
+            }
+            else
+            {
+                ViewBag.GuessedNumber = guessedNumber;
+                ViewBag.errorMsg = "";
+                if (guessedNumber < drawnNumber) ViewBag.GuessingResult = GuessingResult.TooSmall;
+                else if (guessedNumber > drawnNumber) ViewBag.GuessingResult = GuessingResult.TooLarge;
+                else
+                {
+                    ViewBag.GuessingResult = GuessingResult.Perfect;
+                    drawn = false;
+                }
+            }
+
+            return View("Guess");
+        }
+    }
+}
---
+++ lab10/lab10/Controllers/ToolController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab10.Controllers
+{
+    public class ToolController : Controller
+    {
+        [Route("Tool/Solve/{a}/{b}/{c}")]
+        [Route("Tool/Solve/{a}")]
+        [Route("Tool/Solve/{a}/{b}")]
+        public IActionResult Solve(double a, double b = 0, double c = 0)
+        {
+            StringBuilder equation = new StringBuilder();
+
+            if (a != 0)
+            {
+                equation.Append($"{a}x^2");
+                if (b != 0) equation.Append(b > 0 ? $"+{b}x" : $"{b}x");
+                if (c != 0) equation.Append(c > 0 ? $"+{c}" : $"{c}");
+
+
+                double delta = b * b - 4 * a * c;
+                if (delta > 0)
+                {
+                    double sqrtDelta = Math.Sqrt(delta);
+                    double x1 = (-b - sqrtDelta) / (2 * a);
+                    double x2 = (-b + sqrtDelta) / (2 * a);
+                    ViewBag.NumberOfSolutions = 2;
+                    ViewBag.X1 = $"{x1:G5}";
+                    ViewBag.X2 = $"{x2:G5}";
+                }
+                else if (delta == 0)
+                {
+                    double x = (-1) * b / (2 * a);
+                    ViewBag.NumberOfSolutions = 1;
+                    ViewBag.X1 = $"{x:G5}";
+                }
+                else
+                {
+                    ViewBag.NumberOfSolutions = 0;
+                }
+            }
+            else
+            {
+                if (b != 0)
+                {
+                    equation.Append($"{b}x");
+                    if (c != 0) equation.Append(c > 0 ? $"+{c}" : $"{c}");
+                    double x = (-c) / b;
+                    ViewBag.NumberOfSolutions = 1;
+                    ViewBag.X1 = $"{x:G5}";
+                }
+                else if (c == 0)
+                {
+                    equation.Append("0");
+                    ViewBag.NumberOfSolutions = "Real numbers";
+                }
+                else
+                {
+                    equation.Append($"{c}");
+                    ViewBag.NumberOfSolutions = 0;
+                }
+
+            }
+            equation.Append(" = 0");
+            ViewBag.Equation = equation.ToString();
+            return View("Solve");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
