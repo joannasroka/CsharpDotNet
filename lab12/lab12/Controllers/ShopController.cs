df3626851ad66607a46d6414cbd1d87345f3e11b
joannasroka <joannasroka98@gmail.com>
2021-01-19 18:23:51 +0100

    EntityFramework

---
+++ lab12/lab12/Controllers/ShopController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab12.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab12.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = await _context.Articles.Include(a => a.Category).ToListAsync();
+            ViewBag.Categories = _context.Categories.ToList();
+            ViewBag.Current = null;
+            return View(appDbContext);
+        }
+
+        public async Task<IActionResult> Category(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articles = await _context.Articles
+                .Include(a => a.Category)
+                .Where(a => a.CategoryId == id)
+                .ToListAsync();
+
+            ViewBag.Current = id;
+            ViewBag.Categories = _context.Categories.ToList();
+            return View("Index", articles);
+        }
+    }
+}
